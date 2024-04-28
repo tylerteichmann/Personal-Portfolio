a534df352658a60544e712b824c82aeca930ab9a
tylerteichmann <tyler.j.teichmann@gmail.com>
2024-04-28 16:44:43 -0700

    Initialized Spring Project

---
+++ src/main/java/com/example/PersonalPortfolio/PersonalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.PersonalPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPortfolioApplication.class, args);
+	}
+
+}
