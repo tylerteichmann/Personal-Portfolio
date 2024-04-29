19d14060a7dbba52b815fe2a5433bac796531346
tteichmann <tyler.teichmann@stmartin.edu>
2024-04-29 13:13:51 -0700

    completed about page, added a docs folfer

--- src/main/java/com/example/PersonalPortfolio/PersonalPortfolioApplication.java
+++ src/main/java/com/example/PersonalPortfolio/PersonalPortfolioApplication.java
@@ -1,3 +1,5 @@
+// Tyler J. Teichmann
+
 package com.example.PersonalPortfolio;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonalPortfolioApplication {
 
+    // Run the personal portfolio application
     public static void main(String[] args) {
         SpringApplication.run(PersonalPortfolioApplication.class, args);
     }
